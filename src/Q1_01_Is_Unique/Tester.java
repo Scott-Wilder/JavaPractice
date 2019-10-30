b45af6f941fbb6245b327bc74e699601eb896c48
swilde6 <scottwilder3@gmail.com>
2019-10-29 18:09:38 -0700

    Solution for an algorithm to determine if a string has all unique characters.

---
+++ src/Q1_01_Is_Unique/Tester.java
@@ -0,0 +1,16 @@
+package Q1_01_Is_Unique;
+
+public class Tester {
+    public static void main(String[] args) {
+        String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+        for (String word : words) {
+            boolean wordA =  Question1.isUniqueChars(word);
+            boolean wordB =  Question2.isUniqueChars(word);
+            if (wordA == wordB) {
+                System.out.println(word + ": " + wordA);
+            } else {
+                System.out.println(word + ": " + wordA + " vs " + wordB);
+            }
+        }
+    }
+}
